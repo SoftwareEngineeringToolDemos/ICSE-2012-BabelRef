53f4706903e1ec7d738c1677cf2ecc4c83e06388
Mohammed Hasibul Hassan <mhhassan@ncsu.edu>
2015-11-17 17:36:54 -0500

    Updated scripts

--- build-vm/vm-contents/screenlock.sh
+++ build-vm/vm-contents/screenlock.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-sudo /usr/bin/gsettings set org.gnome.desktop.lockdown disable-lock-screen true
-sudo /usr/bin/gsettings set org.gnome.desktop.screensaver lock-enabled false
+/usr/bin/gsettings set org.gnome.desktop.lockdown disable-lock-screen true
+/usr/bin/gsettings set org.gnome.desktop.screensaver lock-enabled false
